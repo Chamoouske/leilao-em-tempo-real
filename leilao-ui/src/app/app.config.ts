62dcf7e331131adf43dba1deb789d6cd38bee25b
Ajax Lima <ajaxlima94@gmail.com>
2025-04-23 20:34:10 -0300

    build: ajustando o build do front-end para funcionar no nginx

--- leilao-ui/src/app/app.config.ts
+++ leilao-ui/src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpLink, InMemoryCache } from '@apollo/client';
+import { environment } from '../environments/environment';
+import { provideApollo } from 'apollo-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideApollo(() => ({
+      link: new HttpLink({
+        uri: environment.graphQLUri,
+      }),
+      cache: new InMemoryCache()
+    }))
+  ]
 };
