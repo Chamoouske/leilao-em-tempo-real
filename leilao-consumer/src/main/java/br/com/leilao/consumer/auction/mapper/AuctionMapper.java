86fc177aa2bc5980590ffabcb9550ddbcdd18d9f
Ajax Lima <ajaxlima94@gmail.com>
2025-04-25 20:57:43 -0300

    feat: adiciona configuração do consumidor de leilão no docker-compose e implementa mapeamento de versão e lance atual com valores padrão no AuctionMapper

--- leilao-consumer/src/main/java/br/com/leilao/consumer/auction/mapper/AuctionMapper.java
+++ leilao-consumer/src/main/java/br/com/leilao/consumer/auction/mapper/AuctionMapper.java
@@ -1,5 +1,6 @@
 package br.com.leilao.consumer.auction.mapper;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -10,13 +11,15 @@ import org.mapstruct.factory.Mappers;
 import br.com.leilao.consumer.auction.dto.AuctionDto;
 import br.com.leilao.consumer.auction.entity.Auction;
 
-@Mapper
+@Mapper(config = AuctionMapperConfig.class)
 public interface AuctionMapper {
     AuctionMapper INSTANCE = Mappers.getMapper(AuctionMapper.class);
+    Long DEFAULT_VERSION = 1L;
+    BigDecimal DEFAULT_CURRENT_BID = BigDecimal.ZERO;
 
-    @Mapping(target = "version", expression = "java(mapVersion(auctionDto.version()))")
+    @Mapping(target = "version", defaultExpression = "java(DEFAULT_VERSION)")
+    @Mapping(target = "currentBid", defaultExpression = "java(DEFAULT_CURRENT_BID)")
     @Mapping(target = "createdAt", expression = "java(mapLocalDateTime(auctionDto.createdAt()))")
-    @Mapping(target = "currentBid", expression = "java(mapCurrentBid(auctionDto.currentBid()))")
     Auction toEntity(AuctionDto auctionDto);
 
     AuctionDto toDto(Auction auction);
@@ -24,15 +27,5 @@ public interface AuctionMapper {
     default LocalDateTime mapLocalDateTime(LocalDateTime localDateTime) {
         Optional<LocalDateTime> offsetDateTime = Optional.ofNullable(localDateTime);
         return offsetDateTime.orElseGet(LocalDateTime::now);
-    }
-
-    default Long mapVersion(Long version) {
-        Optional<Long> offsetVersion = Optional.ofNullable(version);
-        return offsetVersion.orElse(1L);
-    }
-
-    default Long mapCurrentBid(Long currentBid) {
-        Optional<Long> offsetCurrentBid = Optional.ofNullable(currentBid);
-        return offsetCurrentBid.orElse(0L);
     }
 }
